e0d35d9268af0f4147ecc6bac3874e1470493c40
CakemanII <tyler.m.jundt@icloud.com>
2025-11-28 13:06:04 -0500

    small fix.

--- src/main/ts/global_preferences.ts
+++ src/main/ts/global_preferences.ts
@@ -26,7 +26,7 @@ class Preferences
         // Load initial preferences from server.
         setTimeout(() => {
             this.loadPreferencesFromServer();
-        }, 100);
+        }, 0);
     }
 
     /**
