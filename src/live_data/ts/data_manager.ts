70764f06d21ebacf6718ec26a8be02b6ea591de9
CakemanII <tyler.m.jundt@icloud.com>
2025-12-25 19:24:09 -0500

    fixing radio data parsing issue in simulation server

--- src/live_data/ts/data_manager.ts
+++ src/live_data/ts/data_manager.ts
@@ -22,7 +22,7 @@ class LiveDataManager {
         const graphsToCreate: { key: string; title: string; unit: string; yMin: number; yMax: number; }[] = [
             { key: 'accel', title: 'Vertical Acceleration', unit: 'm/s²', yMin: -100, yMax: 100 },
             { key: 'vel', title: 'Vertical Velocity', unit: 'm/s', yMin: -200, yMax: 200 },
-            { key: 'alt', title: 'Altitude', unit: 'm', yMin: -900, yMax: 900 }
+            { key: 'dps_alt', title: 'Altitude', unit: 'm', yMin: -900, yMax: 900 }
         ];
 
         graphsToCreate.forEach(graphInfo => {
--- src/server/radio_communication_simulation_server.py
+++ src/server/radio_communication_simulation_server.py
@@ -28,19 +28,18 @@ class RadioComsSimulationServer:
             data = request.get_json(force=True)
             
             # Format data content
-            data_data: any = data.get("data", {})
-            if isinstance(data_data, dict):
-                data_data = list(data_data.values())[0]
-            else:
-                TypeError("Data content is not in expected format")
+            data_data = data.get("data", {})
+            print(data_data)
+            data_in_data: any = data_data.get(data_data.get("type", None), None)
+            print(data_in_data)
 
             packet: RadioDataObject = {
                 "label": data.get("label", "unknown"),
                 "sent_timestamp": data.get("sent_timestamp", 0.0),
                 "received_timestamp": time.time(),
-                "data": data_data
+                "data": data_in_data
             }
-
+            
             if self._on_receive_radio_data:
                 self._on_receive_radio_data(packet)
 
