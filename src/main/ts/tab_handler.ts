d800a18e3f7bc021d5cce7d6e2e9e4d1f4d5a082
CakemanII <tyler.m.jundt@icloud.com>
2026-03-07 19:24:21 -0500

    temporarily removed live data tab

--- src/main/ts/tab_handler.ts
+++ src/main/ts/tab_handler.ts
@@ -2,7 +2,7 @@ export class TabHandler {
     private static readonly TAB_IDS: Record<string, { tabId: string; buttonId: string }> = {
         liveInterface:   { tabId: 'live_interface_tab',   buttonId: 'live_interface_tab_button' },
         interfaceEditor: { tabId: 'interface_editor_tab', buttonId: 'interface_editor_tab_button' },
-        liveData:        { tabId: 'live_data_tab',        buttonId: 'live_data_tab_button' },
+        //liveData:        { tabId: 'live_data_tab',        buttonId: 'live_data_tab_button' },
         statusEditor:    { tabId: 'status_editor_tab',     buttonId: 'status_editor_tab_button' },
         geofenceEditor:  { tabId: 'geofence_editor_tab',  buttonId: 'geofence_editor_tab_button' },
         preferences:     { tabId: 'preferences_tab',      buttonId: 'preferences_tab_button' },
@@ -33,10 +33,10 @@ export class TabHandler {
                 button: document.getElementById(TabHandler.TAB_IDS.liveInterface.buttonId) as HTMLButtonElement 
             },
 
-            liveData: { 
+            /*liveData: { 
                 content: document.getElementById(TabHandler.TAB_IDS.liveData.tabId) as HTMLIFrameElement, 
                 button: document.getElementById(TabHandler.TAB_IDS.liveData.buttonId) as HTMLButtonElement 
-            },
+            },*/
 
             interfaceEditor: { 
                 content: document.getElementById(TabHandler.TAB_IDS.interfaceEditor.tabId) as HTMLIFrameElement, 
